252f0e805c1150cfff19479c5ac4c43abac30ef6
Dragon Warrior <sekedus@gmail.com>
2018-11-13 03:34:39 +0800

    AADB Images

---
+++ addb.js
@@ -0,0 +1,9 @@
+var aadb = {
+    "images": [{
+        "src": "https://googleads.g.doubleclick.net/favicon.ico"
+    }, {
+        "src": "https://admedia.com/images/favicon.ico"
+    }, {
+        "src": "https://mellowads.com/favicon.ico"
+    }]
+}
