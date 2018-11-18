781768a1dfae44c0f501d34b5cf0afd2e66ff303
Dragon Warrior <sekedus@gmail.com>
2018-11-18 15:25:22 +0800

    Simple Anti Adblock

--- aadb.js
+++ aadb.js
@@ -1,7 +1,5 @@
 keepStart({
     "images": [{
-        "src": "https://googleads.g.doubleclick.net/favicon.ico"
-    }, {
         "src": "https://admedia.com/images/favicon.ico"
     }, {
         "src": "https://mellowads.com/favicon.ico"
