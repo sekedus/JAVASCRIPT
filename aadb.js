6f318eb2f82140746a5740d6d65cbe7de382304c
Dragon Warrior <sekedus@gmail.com>
2018-12-08 06:00:52 +0800

    Update aadb.js

--- aadb.js
+++ aadb.js
@@ -1,9 +1,22 @@
 keepStart({
-    "images": [{
-        "src": "http://admedia.com/images/favicon.ico",
-        "title": "AdMedia"
-    }, {
-        "src": "https://mellowads.com/favicon.ico",
-        "title": "Mellow Ads"
-    }]
-})
+  "javascript": [{
+    "src": "//c1.popads.net/pop.js",
+    "title": "PopAds"
+  }, {
+    "src": "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+    "title": "Google Ads"
+  }],
+  "images": [{
+    "src": "https://www.popads.net/favicon.ico",
+    "title": "PopAds"
+  }, {
+    "src": "http://admedia.com/images/favicon.ico",
+    "title": "AdMedia"
+  }, {
+    "src": "https://mellowads.com/favicon.ico",
+    "title": "Mellow Ads"
+  }, {
+    "src": "http://googleads.g.doubleclick.net/favicon.ico",
+    "title": "DoubleClick"
+  }]
+});
